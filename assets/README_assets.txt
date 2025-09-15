5950c8ec33de1351436bfa0748126bba010b72bf
Assela Pathirana <apa@un-ihe.org>
2025-09-15 09:46:31 +0200

    Files added

---
+++ assets/README_assets.txt
@@ -0,0 +1,1 @@
+Put your PDFs, slides, and other downloadable files in this folder and link them from index.html.
---
+++ data/videos.js
@@ -0,0 +1,11 @@
+// Update this list to add/remove entries.
+// You can also use objects like {id: "VIDEO_ID", title: "Optional custom title"}.
+const VIDEOS = [
+  "lyCTxfHT7cs",
+  "mwO-bwVDDck",
+  "vCoyIAYmykw",
+  "I6JRcArzcBE",
+  "yoGPkRaqa5Q",
+  "uWTJxrJfuQA",
+  "wkr61OC9Ejk"
+];
